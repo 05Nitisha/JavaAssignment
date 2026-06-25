f8f1cbec789ab0d9190083937e64600b23d37592
nitisha kamboj <kambojtashu@gmail.com>
2026-06-25 19:35:35 +0530

    If Else added

---
+++ Assignment3_IfElse.java
@@ -0,0 +1,34 @@
+package week1;
+
+public class Assignment3_IfElse {
+	public static void main(String[] args) {
+	
+	int num=100;
+	int num1=76;
+	int num3=68;
+	//int num4=48;
+	int num4=20;
+	
+	if (num>=90 && num<=100)
+	{	
+	System.out.println("A");
+	}
+	if (num1>=75 && num1<=89)
+	{
+	System.out.println("B");
+	}
+  if (num3 >=60 && num3<=74)
+		{
+		System.out.println("C");
+		}
+	
+	 if (num4>=40 && num4<=59)
+	{ 
+		System.out.println("D");
+	}
+	
+	else 
+	{
+		System.out.println("Fail");
+	}
+	}}
