4e515001e8b31ebfd6ffaee983a90545ccdaefc7
nitisha kamboj <kambojtashu@gmail.com>
2026-07-02 15:52:30 +0530

    Assignment done

---
+++ Assignment_AlteranateupperCase.java
@@ -0,0 +1,18 @@
+package week1;
+
+public class Assignment_AlteranateupperCase {
+
+	
+
+	public static void main(String[] args) {
+		char alphabet='A';
+			
+		while(alphabet<='Z')
+		{
+			System.out.print(alphabet + " ");
+			alphabet+=2;
+		}
+
+	}
+
+}
