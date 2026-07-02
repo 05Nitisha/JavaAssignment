0715c62483aecf5f3e4e22ffde7f081b7500497e
nitisha kamboj <kambojtashu@gmail.com>
2026-07-02 15:54:54 +0530

    Assignment done

---
+++ Assignment_ASCIIUsingWhile.java
@@ -0,0 +1,19 @@
+package week1;
+
+public class Assignment_ASCIIUsingWhile {
+
+	
+
+	public static void main(String[] args) {
+		char alphabet='A';
+			
+		while(alphabet<='Z')
+		{
+			int NewValue=(int)alphabet;
+			System.out.println((char)+alphabet+ "=" +NewValue);
+			alphabet++;
+		}
+	}
+	}
+
+
