6878ece9202b2b7d783b0ac96d336fdad14eab72
05Nitisha <kambojtashu@gmail.com>
2026-07-07 01:17:32 +0530

    Add files via upload

---
+++ Program1_RemoveDuplicatePrint.java
@@ -0,0 +1,34 @@
+package week2;
+
+public class Program1_RemoveDuplicatePrint {
+
+	public static void main(String[] args) {
+		int arr[]= {5,3,8,5,2,3,9,8};
+		
+		
+		for(int i=0;i<arr.length;i++) 
+		{
+			
+			boolean isDuplicate=false;
+		
+			for(int j=0;j<i;j++)
+			{
+				if(arr[i]==arr[j]) {
+					isDuplicate=true;
+					
+				break;
+			}
+			}
+			
+				if(isDuplicate==false)
+			
+			System.out.print(arr[i] + " ");
+		
+		}
+	}
+}
+
+
+	
+
+
